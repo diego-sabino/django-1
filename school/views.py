ca83b0db879f57a7ea6a5513bad046bcb4c0df10
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-16 17:18:02 -0300

    atividade 1

--- school/views.py
+++ school/views.py
@@ -1,6 +1,17 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Course
+from serializer import StudentSerializer, CourseSerializer
 
-def students(request):
-  if request.method == 'GET':
-    student = {'id': 1, 'name': 'zephyr664'}
-    return JsonResponse(student)
+class StudentsViewSet(viewsets.ModelViewSet):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+
+# def students(request):
+#   if request.method == 'GET':
+#     student = {'id': 1, 'name': 'zephyr664'}
+#     return JsonResponse(student)
