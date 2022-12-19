5ccb9f615a96a7dae1ecbd4dcae3784c84ec6033
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-19 18:12:05 -0300

    atividade 3

--- school/views.py
+++ school/views.py
@@ -2,10 +2,14 @@ from django.http import JsonResponse
 from rest_framework import viewsets, generics
 from school.models import Student, Course, Registration
 from school.serializer import StudentSerializer, CourseSerializer, RegistrationSerializer, RegistrationStudentSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class StudentsViewSet(viewsets.ModelViewSet):
   queryset = Student.objects.all()
   serializer_class = StudentSerializer
+  authentication_classes = [BasicAuthentication]
+  permission_classes = [IsAuthenticated]
 
 class CourseViewSet(viewsets.ModelViewSet):
   queryset = Course.objects.all()
